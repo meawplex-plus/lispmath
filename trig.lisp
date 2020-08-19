0a2f71706fe9f32ea43d32fa49923a85aab3aef1
Mo Talafuse <62037395+mohindertalafuse@users.noreply.github.com>
2020-08-19 08:16:38 -0500

    Add complementary co-__ functions

--- trig.lisp
+++ trig.lisp
@@ -1,5 +1,5 @@
 (defun hypot (leg1 leg2)
-  "Calculate hypotenuse length of two legs."
+  "Calculate hypotenuse length of triangle made by two legs."
   (sqrt (+ (* leg1 leg1) (* leg2 leg2))))
 
 (defun sin-degrees (degrees)
@@ -16,3 +16,17 @@ Returns number of type double-float; in other words, (cos-degrees 360) -> 1.0d0.
   "Calculate the ratio of the opposite to the adjacent leg of the angle chosen.
 Returns number of type double-float; in other words, (tan-degrees 45) -> 1.0d0."
   (tan (* (/ pi 180) degrees)))
+
+(defun sec-degrees (degrees)
+  "Calulate the secant of an angle, which is 1/cos(angle). 
+  Returns number of type double float; in other words, (sec-degrees 0) -> 1.0d0."
+  (/ 1 (cos-degrees angle)))
+
+(defun cotan-degrees (degrees)
+  "Calculate the cotangent of an angle, which is 1/tan(angle).
+  Returns number of type double float; in other words, (cotan-degrees 45) -> 1.0d0."
+  (/ 1 (tan-degrees degrees)))
+
+(defun cosec-degrees (degrees)
+  "Calculate the cotangent of an angle, which is 1/tan(angle).
+  Returns number of type double float; in other words, (cosec-degrees 90) -> 1.0d0."
