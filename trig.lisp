f9e0d8b87ae1919b464dcd0e76a5e732397e0174
Mo Talafuse <62037395+mohindertalafuse@users.noreply.github.com>
2020-08-19 08:19:24 -0500

    Add new complementary co-__ functions

--- trig.lisp
+++ trig.lisp
@@ -18,7 +18,7 @@ Returns number of type double-float; in other words, (tan-degrees 45) -> 1.0d0."
   (tan (* (/ pi 180) degrees)))
 
 (defun sec-degrees (degrees)
-  "Calulate the secant of an angle, which is 1/cos(angle). 
+  "Calulate the secant (colloquially the 'co-cosine') of an angle, which is 1/cos(angle). 
   Returns number of type double float; in other words, (sec-degrees 0) -> 1.0d0."
   (/ 1 (cos-degrees angle)))
 
@@ -28,5 +28,5 @@ Returns number of type double-float; in other words, (tan-degrees 45) -> 1.0d0."
   (/ 1 (tan-degrees degrees)))
 
 (defun cosec-degrees (degrees)
-  "Calculate the cotangent of an angle, which is 1/tan(angle).
+  "Calculate the cosecant of an angle, which is 1/sec(angle).
   Returns number of type double float; in other words, (cosec-degrees 90) -> 1.0d0."
