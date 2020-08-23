885ab69b5fc0ae2e19f9f8b7d55a3747feee028c
Mo Talafuse <62037395+mohindertalafuse@users.noreply.github.com>
2020-08-23 10:33:55 -0500

    Public API 1
    The docs are now fixed! I can't wait to add more over time, and please welcome the release of lispmath 1.0.0 public API 1!

--- package.lisp
+++ package.lisp
@@ -1,5 +1,7 @@
 (in-package #:cl-user)
 (defpackage #:lispmath
+  (:nicknames "GitHub at https://github.com/mohindertalafuse/lispmath")
+  (:documentation "Handy math tools for Common Lisp.")
   (:use #:cl)
   (:export
    #:sin-degrees
