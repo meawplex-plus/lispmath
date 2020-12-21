d3b206b088bc249588d117f0be6f5c5358b0e88c
Karsten Johansson <KarstenJohansson@gmail.com>
2020-12-21 10:39:56 -0500

    Karsten Additions
    Added circular.lisp and reflected additions in lispmath.asd and package.lisp

---
+++ circular.lisp
@@ -0,0 +1,58 @@
+(in-package #:lispmath)
+
+;; Some useful constants, no earmuffs so they are more like pi
+(defconstant tau (+ pi pi) "Tau is actually used more often than pi.")
+(defconstant τ   tau       "Some people like to use symbols.")
+(defconstant 2π  tau       "Same with its common nickname.")
+(defconstant 2pi tau       "And this one.")
+(defconstant π   pi        "And lest we forget...")
+
+;;
+(defun ellipse-perimeter (a b)
+ "Matt Parker's algorithm for estimating the perimeter of an ellipse,
+  where perimeter ≈ π[53a/3 + 717b/35 - √(269a^2 + 667ab + 371b^2)]
+Implemented by Karsten Johansson https://github.com/ksaj/ksaj-ellipses"
+
+  (check-type a number)   ; Keep code safe from injection
+  (check-type b number)   ; 
+
+  (if (= a b)
+    (princ "Use the circumference function for circles. This result will be inaccurate."))
+
+  (if (< a b)             ; A needs to be the longer axis
+    (rotatef a b))
+
+  (* pi (- (+ (* 53  (/ a 3))
+              (* 717 (/ b 35)))
+
+           (sqrt (+ (* 269 (* a a))
+                    (* 667 a b)
+                    (* 371 (* b b)))))))
+
+;;
+(defun ellipse-area (a b)
+ "Calculate Area of an Ellipse. Implemented by Kasten Johansson."
+
+  (check-type a number)   ; Keep code safe from injection
+  (check-type b number)   ;
+
+  (* pi a b))
+
+;;
+(defun circle-area (radius)
+ "Calculate Area of a Circle. Implemented by Karsten Johansson."
+
+  (check-type radius number)
+
+  (* pi radius radius))
+
+;;
+(defun circumference (radius)
+ "Calculate Circumference of a Circle"
+
+  (check-type radius number)
+
+  (* tau radius))
+
+
+
--- lispmath.asd
+++ lispmath.asd
@@ -1,11 +1,12 @@
 (asdf:defsystem #:lispmath
     :version "1.0.0"
     :description "PUBLIC API 1"
-    :author "mohindertalafuse (Mo Talafuse) and stalafuse (Scott Talafuse)"
+    :author "mohindertalafuse (Mo Talafuse) and stalafuse (Scott Talafuse) and ksaj (Karsten Johansson)"
     :homepage "https://mohindertalafuse.github.io"
     :bug-tracker "https://github.com/mohindertalafuse/lispmath/issues"
     :source-control (:git "https://github.com/mohindertalafuse/lispmath")
     :serial t
     :components ((:file "package")
 		 (:file "functions-and-predicates")
-		 (:file "trig")))
+		 (:file "trig")
+                 (:file "circular")))
