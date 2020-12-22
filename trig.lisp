ad7668247a51ba6c847f70ac6e0c5f8c1207753f
Mo Talafuse <62037395+mohindertalafuse@users.noreply.github.com>
2020-12-22 14:10:51 -0600

    new features

--- trig.lisp
+++ trig.lisp
@@ -1,5 +1,13 @@
 (in-package #:lispmath)
 
+
+(defparameter *sin-degrees-identity* 90)
+(defparameter *cos-degrees-identity* 360)
+(defparameter *tan-degrees-identity* 45)
+(defparameter *sec-degrees-identity* 0)
+(defparameter *cot-degrees-identity* 45)
+(defparameter *csc-degrees-identity* 90)
+
 (defun hypot (leg1 leg2)
   "Calculate hypotenuse length of triangle made by two legs."
   (sqrt (+ (* leg1 leg1) (* leg2 leg2))))
@@ -24,12 +32,12 @@ Returns number of type double-float; in other words, (tan-degrees 45) -> 1.0d0."
 Returns number of type double float; in other words, (sec-degrees 0) -> 1.0d0."
   (/ 1 (cos-degrees degrees)))
 
-(defun cotan-degrees (degrees)
+(defun cot-degrees (degrees)
   "Calculate the cotangent of an angle, which is 1/tan(angle).
-Returns number of type double float; in other words, (cotan-degrees 45) -> 1.0d0."
+Returns number of type double float; in other words, (cot-degrees 45) -> 1.0d0."
   (/ 1 (tan-degrees degrees)))
 
-(defun cosec-degrees (degrees)
+(defun csc-degrees (degrees)
   "Calculate the cosecant of an angle, which is 1/sin(angle).
-Returns number of type double float; in other words, (cosec-degrees 90) -> 1.0d0."
+Returns number of type double float; in other words, (csc-degrees 90) -> 1.0d0."
   (/ 1 (sin-degrees degrees)))
