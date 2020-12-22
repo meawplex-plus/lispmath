2839f2557a7a415e1151f7c26d20e2484b8bb95d
Mo Talafuse <62037395+mohindertalafuse@users.noreply.github.com>
2020-12-22 14:10:51 -0600

    new features

--- staple.ext.lisp
+++ staple.ext.lisp
@@ -1,6 +1,8 @@
 (defmethod staple:template ((system (eql (asdf:find-system :lispmath))))
   (asdf:system-relative-pathname system #p"template.ctml"))
 (defclass my-page (staple:simple-page) ())
+(defmethod staple:filename ((page my-page))
+  (make-pathname :name "defindexes" :type "html"))
 (defmethod staple:page-type ((system (eql (asdf:find-system :lispmath)))) 'my-page)
 (defmethod staple:resolve-source-link (source (page my-page))
   (format NIL "http://github.com/mohindertalafuse/lispmath/blob/master/~a.lisp#L~a" 
